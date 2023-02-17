4a504a3c64425b2dfb1a8ab6233021424db53b1a
hleb <hleb@hleb.com>
2023-02-17 11:32:07 +0300

    *Installed and configured pytest-django *Created tests for user app *Added view (will be reworked in future) and url for user verification *Reworked user creation View due to incorrect password setting *Corrected minor problems (revealed while tests creation)
    **Reworked tests with the use of fixtures
    **Added CustomUserRUD Serializer for correct fields use in CustomUserRUDView
    **Added user_fixtures file (with all user fixtures)
    **Added conftest file to set fixtures files to autorun at tests start
    **Minor code fixes
    
    ***Reworked fixtures to work with ORM (not requests)
    ***Added RequestClient fixture
    ***All_users and All_profiles fixtures changed to function scope
    ***Mass response renaming and minor cleanups
    
    ****Changed test urls from absolute to reverse("name")

--- project/user/tests.py
+++ project/user/tests.py
@@ -2,8 +2,9 @@ import uuid
 from random import choice
 
 import pytest
+from django.urls import reverse
 
-BASE_URL = 'http://testserver/users/'
+BASE_URL = 'http://testserver'
 
 pytestmark = pytest.mark.django_db
 
@@ -12,7 +13,7 @@ pytestmark = pytest.mark.django_db
                          ['DEALER', 'SELLER', 'BUYER'],
                          indirect=True)
 def test_create_users_with_different_types(user_data, client):
-    response = client.post(f'{BASE_URL}registration/',
+    response = client.post(f'{BASE_URL}{reverse("registration")}',
                            json=user_data)
     assert response.status_code == 201
     assert response.json()['username'] == user_data['username']
@@ -23,17 +24,17 @@ def test_create_users_with_different_types(user_data, client):
                          ['DEALER', 'SELLER', 'BUYER'],
                          indirect=True)
 def test_user_can_read_update_delete_his_user_profile(unverified_user, client):
-    response = client.get(f'{BASE_URL}my_user_page/',
+    response = client.get(f'{BASE_URL}{reverse("user")}',
                           headers=unverified_user['headers'])
     assert response.status_code == 200
     assert response.json()['username'] == unverified_user['created_user']['username']
     unverified_user['created_user']['username'] = f'PreDelete{uuid.uuid4().hex}'
-    response = client.put(f'{BASE_URL}my_user_page/',
+    response = client.put(f'{BASE_URL}{reverse("user")}',
                           headers=unverified_user['headers'],
                           json=unverified_user['created_user'])
     assert response.status_code == 200
     assert response.json()['username'] == unverified_user['created_user']['username']
-    response = client.delete(f'{BASE_URL}my_user_page/',
+    response = client.delete(f'{BASE_URL}{reverse("user")}',
                              headers=unverified_user['headers'])
     assert response.status_code == 200
 
@@ -42,13 +43,13 @@ def test_user_can_read_update_delete_his_user_profile(unverified_user, client):
                          [choice(['DEALER', 'SELLER', 'BUYER'])],
                          indirect=True)
 def test_created_user_can_log_in_and_refresh(user_data, client):
-    client.post(f'{BASE_URL}registration/', json=user_data)
-    response = client.post(f'{BASE_URL}login/',
+    client.post(f'{BASE_URL}{reverse("registration")}', json=user_data)
+    response = client.post(f'{BASE_URL}{reverse("get-token")}',
                            json=user_data)
     refresh = response.json()['refresh']
     assert response.status_code == 200
     assert response.json()['access']
-    response = client.post(f'{BASE_URL}login/refresh_token/',
+    response = client.post(f'{BASE_URL}{reverse("refresh-token")}',
                            json={'refresh': refresh})
     assert response.status_code == 200
     assert response.json()['access']
@@ -59,10 +60,10 @@ def test_created_user_can_log_in_and_refresh(user_data, client):
                          indirect=True)
 def test_inactive_user_can_not_login_and_refresh(unverified_user, client):
     unverified_user['created_user']['is_active'] = False
-    client.put(f'{BASE_URL}my_user_page/',
+    client.put(f'{BASE_URL}{reverse("user")}',
                headers=unverified_user['headers'],
                json=unverified_user['created_user'])
-    response = client.post(f'{BASE_URL}login/',
+    response = client.post(f'{BASE_URL}{reverse("get-token")}',
                            json=unverified_user['creation_data'])
     assert response.status_code == 401
 
@@ -72,7 +73,7 @@ def test_inactive_user_can_not_login_and_refresh(unverified_user, client):
                          indirect=True)
 def test_user_can_be_verified(unverified_user, client):
     id_data = {'user_id': unverified_user['created_user']['id']}
-    response = client.put(f'{BASE_URL}verification/',
+    response = client.put(f'{BASE_URL}{reverse("verification")}',
                           json=id_data)
     assert response.status_code == 200
     assert response.json()['is_verified']
@@ -80,7 +81,7 @@ def test_user_can_be_verified(unverified_user, client):
 
 @pytest.mark.parametrize('unverified_user', ['DEALER'], indirect=True)
 def test_unverified_users_can_not_create_profile(unverified_user, dealer_profile, client):
-    response = client.post(f'{BASE_URL}create_dealer_profile/',
+    response = client.post(f'{BASE_URL}{reverse("dealer-creation")}',
                            json=dealer_profile,
                            headers=unverified_user['headers'])
     assert response.status_code == 403
@@ -88,7 +89,7 @@ def test_unverified_users_can_not_create_profile(unverified_user, dealer_profile
 
 @pytest.mark.parametrize('verified_user', ['DEALER'], indirect=True)
 def test_verified_users_can_create_profile(verified_user, dealer_profile, client):
-    response = client.post(f'{BASE_URL}create_dealer_profile/',
+    response = client.post(f'{BASE_URL}{reverse("dealer-creation")}',
                            json=dealer_profile,
                            headers=verified_user['headers'])
     assert response.status_code == 201
@@ -105,12 +106,14 @@ def test_users_with_different_types_can_create_only_corresponding_profile(verifi
     for user_type, profile in {'dealer': dealer_profile,
                                'seller': seller_profile,
                                'buyer': buyer_profile}.items():
-        response = client.post(f'{BASE_URL}create_{user_type}_profile/',
+        url_name = f'{user_type}-creation'
+        response = client.post(f'{BASE_URL}{reverse(url_name)}',
                                json=profile,
                                headers=verified_user['headers'])
         if user_type == str.lower(verified_user['created_user']['user_type']):
             assert response.status_code == 201
-            response = client.get(f'{BASE_URL}my_{user_type}_profile/',
+            url_name = f'{user_type}-profile'
+            response = client.get(f'{BASE_URL}{reverse(url_name)}',
                                   headers=verified_user['headers'])
             assert response.status_code == 200
         else:
@@ -125,21 +128,23 @@ def test_dealer_seller_can_read_update_delete_his_type_profile(verified_user,
                                                                client):
     user_type = str.lower(verified_user['created_user']['user_type'])
     profile_data = request.getfixturevalue(f'{user_type}_profile')
-    client.post(f'{BASE_URL}create_{user_type}_profile/',
+    url_name = f'{user_type}-creation'
+    client.post(f'{BASE_URL}{reverse(url_name)}',
                 json=profile_data,
                 headers=verified_user['headers'])
-    response = client.get(f'{BASE_URL}my_{user_type}_profile/',
+    url_name = f'{user_type}-profile'
+    response = client.get(f'{BASE_URL}{reverse(url_name)}',
                           headers=verified_user['headers'])
     assert response.status_code == 200
     assert response.json()['is_active']
     profile_data = response.json()
     profile_data['name'] = f'PreDelete{uuid.uuid4()}'
-    response = client.put(f'{BASE_URL}my_{user_type}_profile/',
+    response = client.put(f'{BASE_URL}{reverse(url_name)}',
                           headers=verified_user['headers'],
                           json=profile_data)
     assert response.status_code == 200
     assert profile_data['name'] == response.json()['name']
-    response = client.delete(f'{BASE_URL}my_{user_type}_profile/',
+    response = client.delete(f'{BASE_URL}{reverse(url_name)}',
                              headers=verified_user['headers'])
     assert response.status_code == 200
 
@@ -152,21 +157,21 @@ def test_buyer_can_read_update_delete_his_type_profile(verified_user,
                                                        client):
     user_type = str.lower(verified_user['created_user']['user_type'])
     user_profile = request.getfixturevalue(f'{user_type}_profile')
-    client.post(f'{BASE_URL}create_{user_type}_profile/',
+    client.post(f'{BASE_URL}{reverse("buyer-creation")}',
                 json=user_profile,
                 headers=verified_user['headers'])
-    response = client.get(f'{BASE_URL}my_{user_type}_profile/',
+    response = client.get(f'{BASE_URL}{reverse("buyer-profile")}',
                           headers=verified_user['headers'])
     assert response.status_code == 200
     assert response.json()['is_active']
     profile_data = response.json()
     profile_data['firstname'] = f'PreDelete{uuid.uuid4()}'
-    response = client.put(f'{BASE_URL}my_{user_type}_profile/',
+    response = client.put(f'{BASE_URL}{reverse("buyer-profile")}',
                           headers=verified_user['headers'],
                           json=profile_data)
     assert response.status_code == 200
     assert profile_data['firstname'] == response.json()['firstname']
-    response = client.delete(f'{BASE_URL}my_{user_type}_profile/',
+    response = client.delete(f'{BASE_URL}{reverse("buyer-profile")}',
                              headers=verified_user['headers'])
     assert response.status_code == 200
 
@@ -177,11 +182,13 @@ def test_dealer_can_view_other_users(all_users, all_profiles, client):
 
     # Dealers can view all other user profiles
     for user_type in all_users.keys():
-        response = client.get(f'{BASE_URL}{user_type}s/',
+        url_name = f'{user_type}-list'
+        response = client.get(f'{BASE_URL}{reverse(url_name)}',
                               headers=dealer['headers'])
         assert response.status_code == 200
         assert response.json()[0]['id']
-        response = client.get(f'{BASE_URL}{user_type}s/1/',
+        url_name = f'{user_type}-detail'
+        response = client.get(f'{BASE_URL}{reverse(url_name, kwargs={"pk": 1})}',
                               headers=dealer['headers'])
         assert response.status_code == 200
         assert response.json()['id']
@@ -193,19 +200,21 @@ def test_buyer_can_view_other_users(all_users, all_profiles, client):
 
     # Buyers can't view seller and other buyer profiles
     for user_type in ['seller', 'buyer']:
-        response = client.get(f'{BASE_URL}{user_type}s/',
+        url_name = f'{user_type}-list'
+        response = client.get(f'{BASE_URL}{reverse(url_name)}',
                               headers=buyer['headers'])
         assert response.status_code == 403
-        response = client.get(f'{BASE_URL}{user_type}s/1/',
+        url_name = f'{user_type}-detail'
+        response = client.get(f'{BASE_URL}{reverse(url_name, kwargs={"pk": 1})}',
                               headers=buyer['headers'])
         assert response.status_code == 403
 
     # Buyers can view dealer profiles
-    response = client.get(f'{BASE_URL}dealers/',
+    response = client.get(f'{BASE_URL}{reverse("dealer-list")}',
                           headers=buyer['headers'])
     assert response.status_code == 200
     assert response.json()[0]['id']
-    response = client.get(f'{BASE_URL}dealers/1/',
+    response = client.get(f'{BASE_URL}{reverse("dealer-detail", kwargs={"pk": 1})}',
                           headers=buyer['headers'])
     assert response.status_code == 200
     assert response.json()['id']
@@ -217,20 +226,22 @@ def test_seller_can_view_other_users(all_users, all_profiles, client):
 
     # Sellers can view dealers and other sellers profiles
     for user_type in ['seller', 'dealer']:
-        response = client.get(f'{BASE_URL}{user_type}s/',
+        url_name = f'{user_type}-list'
+        response = client.get(f'{BASE_URL}{reverse(url_name)}',
                               headers=buyer['headers'])
         assert response.status_code == 200
         assert response.json()[0]['id']
-        response = client.get(f'{BASE_URL}{user_type}s/1/',
+        url_name = f'{user_type}-detail'
+        response = client.get(f'{BASE_URL}{reverse(url_name, kwargs={"pk": 1})}',
                               headers=buyer['headers'])
         assert response.status_code == 200
         assert response.json()['id']
 
     # Sellers can't view buyer profiles
-    response = client.get(f'{BASE_URL}buyers/',
+    response = client.get(f'{BASE_URL}{reverse("buyer-list")}',
                           headers=buyer['headers'])
     assert response.status_code == 403
-    response = client.get(f'{BASE_URL}buyers/1/',
+    response = client.get(f'{BASE_URL}{reverse("buyer-detail", kwargs={"pk": 1})}',
                           headers=buyer['headers'])
     assert response.status_code == 403
 
@@ -240,27 +251,27 @@ def test_seller_can_view_other_users(all_users, all_profiles, client):
                          indirect=True)
 def test_invalid_data_tests(user_data, dealer_profile, client):
     user_data['user_type'] = 'ABRA_ABRA_CADABRA'
-    response = client.post(f'{BASE_URL}registration/',
+    response = client.post(f'{BASE_URL}{reverse("registration")}',
                            json=user_data)
     assert response.status_code == 400
 
     user_data['user_type'] = 'DEALER'
-    response = client.post(f'{BASE_URL}registration/',
+    response = client.post(f'{BASE_URL}{reverse("registration")}',
                            json=user_data)
     id_data = {'user_id': response.json()['id']}
-    client.put(f'{BASE_URL}verification/', json=id_data)
-    key = client.post(f'{BASE_URL}login/',
+    client.put(f'{BASE_URL}{reverse("verification")}', json=id_data)
+    key = client.post(f'{BASE_URL}{reverse("get-token")}',
                       json=user_data).json()['access']
     headers = {
         'Authorization': f'Bearer {str(key)}'
     }
     dealer_profile['home_country'] = 'ABRA_ABRA_CADABRA'
-    response = client.post(f'{BASE_URL}create_dealer_profile/',
+    response = client.post(f'{BASE_URL}{reverse("dealer-creation")}',
                            json=dealer_profile,
                            headers=headers)
     assert response.status_code == 400
 
     user_data['email'] = 'ABRA_ABRA_CADABRA'
-    response = client.post(f'{BASE_URL}login/',
+    response = client.post(f'{BASE_URL}{reverse("get-token")}',
                            json=user_data)
     assert response.status_code == 401
