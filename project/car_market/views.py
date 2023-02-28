62d82230398d807d1ac4ba23d7a69f6dc588148c
hleb <hleb@hleb.com>
2023-02-28 21:42:34 +0300

    Filters and filters tests for car_market and car_spec

--- project/car_market/views.py
+++ project/car_market/views.py
@@ -1,5 +1,8 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 from root.common.views import BaseReadOnlyView
 
+from .market_filter import CarFilter
 from .models import MarketAvailableCarModel
 from .serializers import MarkerAvailableCarsModelSerializer
 
@@ -7,3 +10,10 @@ from .serializers import MarkerAvailableCarsModelSerializer
 class MarketAvailableCarModelView(BaseReadOnlyView):
     serializer = MarkerAvailableCarsModelSerializer
     model = MarketAvailableCarModel
+    filter_backends = (DjangoFilterBackend,
+                       filters.SearchFilter,
+                       filters.OrderingFilter,)
+    filterset_class = CarFilter
+    search_fields = ['brand_name', 'car_model_name']
+    ordering_fields = ['engine_volume', 'year_of_production',
+                       'demand_level']
