7301f4080e292fcb38326fa851f7f8929e2bec37
hleb <hleb@hleb.com>
2023-03-01 10:36:31 +0300

    Filters and filter tests for users and promo

--- project/promo/views.py
+++ project/promo/views.py
@@ -1,21 +1,32 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 from root.common.permissions import (IsDealer, IsOwnerOrAdmin, IsSeller,
                                      IsVerified)
 from root.common.views import BaseCRUDView, BaseReadOnlyView
 from user.models import AutoDealerModel, AutoSellerModel
 
 from .models import DealerPromoModel, SellerPromoModel
+from .promo_filter import PromoFilterDealer, PromoFilterSeller
 from .serializers import DealersPromoSerializer, SellersPromoSerializer
 
 
 class DealerPromoReadOnlyView(BaseReadOnlyView):
     serializer = DealersPromoSerializer
     model = DealerPromoModel
+    filterset_class = PromoFilterDealer
+    filter_backends = (filters.OrderingFilter,
+                       DjangoFilterBackend,)
+    ordering_fields = ['discount_size']
 
 
 class SellerPromoReadOnlyView(BaseReadOnlyView):
     permission_classes = [IsSeller & IsVerified | IsDealer & IsVerified]
     serializer = SellersPromoSerializer
     model = SellerPromoModel
+    filterset_class = PromoFilterSeller
+    filter_backends = (filters.OrderingFilter,
+                       DjangoFilterBackend,)
+    ordering_fields = ['discount_size']
 
 
 class DealerPromoCRUDView(BaseCRUDView):
@@ -24,6 +35,10 @@ class DealerPromoCRUDView(BaseCRUDView):
     model = DealerPromoModel
     user_data = 'creator'
     user_model = AutoDealerModel
+    filterset_class = PromoFilterDealer
+    filter_backends = (filters.OrderingFilter,
+                       DjangoFilterBackend,)
+    ordering_fields = ['discount_size']
 
 
 class SellerPromoCRUDView(BaseCRUDView):
@@ -32,3 +47,7 @@ class SellerPromoCRUDView(BaseCRUDView):
     model = SellerPromoModel
     user_data = 'creator'
     user_model = AutoSellerModel
+    filterset_class = PromoFilterSeller
+    filter_backends = (filters.OrderingFilter,
+                       DjangoFilterBackend,)
+    ordering_fields = ['discount_size']
